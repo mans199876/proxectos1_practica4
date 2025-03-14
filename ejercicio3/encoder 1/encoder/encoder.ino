d91050fdbcb0660abd370bef5be5e89d685726cc
Yonifarolas <102153795+Yonifarolas@users.noreply.github.com>
2025-03-14 10:55:21 +0100

    Add files via upload

---
+++ ejercicio3/encoder 1/encoder/encoder.ino
@@ -0,0 +1,25 @@
+//Declaramos las variables generales
+const int sensorPin = 2;  // Pin al que está conectado el sensor HW-484
+volatile bool sensorTriggered = false;
+
+//Funciones para la interrupcion
+void sensorISR() {
+  sensorTriggered = true;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sensorPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), sensorISR, RISING);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (sensorTriggered) {
+    Serial.println("Interrupción detectada: Sensor activado");
+    sensorTriggered = false;
+  }
+
+}
